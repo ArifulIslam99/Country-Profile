76e62402f9bd3ab654737bdadfd6aeedbb55df6f
ArifulIslam99 <afridi2000rampura@gmail.com>
2022-07-02 21:43:13 +0600

    dynamic content with intent

--- app/src/main/java/com/example/countryprofile/InformationActiviy.java
+++ app/src/main/java/com/example/countryprofile/InformationActiviy.java
@@ -3,12 +3,45 @@ package com.example.countryprofile;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class InformationActiviy extends AppCompatActivity {
+
+    private ImageView image;
+    private TextView text;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_information_activiy);
+
+        image = findViewById(R.id.image_country);
+        text = findViewById(R.id.text_country);
+
+        Bundle bundle = getIntent().getExtras();
+
+        if(bundle!=null){
+            String countryName = bundle.getString("name");
+            show_details(countryName);
+        }
+
+
+    }
+
+    void show_details(String country_name){
+        if(country_name.equals("bangladesh")){
+            image.setImageResource(R.drawable.dhaka);
+            text.setText(R.string.bangladesh);
+        }
+        else if(country_name.equals("india")){
+            image.setImageResource(R.drawable.agra);
+            text.setText(R.string.india);
+        }
+        else if(country_name.equals("pakistan")){
+            image.setImageResource(R.drawable.islamabad);
+            text.setText(R.string.pakistan);
+        }
+
     }
 }
--- app/src/main/java/com/example/countryprofile/MainActivity.java
+++ app/src/main/java/com/example/countryprofile/MainActivity.java
@@ -2,13 +2,51 @@ package com.example.countryprofile;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private Button bd_btn, ind_btn, pak_btn;
+    private Intent intent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        bd_btn = findViewById(R.id.bd_btn);
+        ind_btn = findViewById(R.id.india_btn);
+        pak_btn = findViewById(R.id.pak_btn);
+
+        bd_btn.setOnClickListener(this);
+        ind_btn.setOnClickListener(this);
+        pak_btn.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+
+
+        if(v.getId() == R.id.bd_btn){
+            intent = new Intent(MainActivity.this, InformationActiviy.class);
+            intent.putExtra("name", "bangladesh");
+            startActivity(intent);
+        }
+
+        else if(v.getId() == R.id.india_btn){
+            intent = new Intent(MainActivity.this, InformationActiviy.class);
+            intent.putExtra("name", "india");
+            startActivity(intent);
+        }
+
+        else if(v.getId() == R.id.pak_btn){
+            intent = new Intent(MainActivity.this, InformationActiviy.class);
+            intent.putExtra("name", "pakistan");
+            startActivity(intent);
+        }
+
     }
 }
